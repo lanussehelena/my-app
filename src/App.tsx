5d7d254969a17099b97aa579bd4d373b64125970
lanussehelena <lanusse.dev@gmail.com>
2026-01-07 10:53:20 -0300

    feat: Adicionado tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { BrowserRouter } from "react-router";
 import { AppRoutes } from "./routes";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { ThemeProvider } from "@emotion/react";
+import { LightTheme } from "./shared/themes";
+
+
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
   
 }
